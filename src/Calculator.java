a0997d253a8cdf0083692db403f627b42f7e12e6
bowen zhang <s3617571@student.rmit.edu.au>
2018-08-13 19:07:13 +1000

    finish

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,41 +1,30 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 public class Calculator {
 	private static int p;
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Scanner sc = new Scanner(System.in);
-		//String input = sc.nextLine();
-		//System.out.print("Enter a number");
-		//int[] inBry = BinaryGenerate(input);
-		//int[] HMcode = HammingCodeGenerate(inBry);
-		//System.out.print(printArray("Bit Convertion: ", inBry) + "\n" + printArray("HammingCode: ", HMcode));
-		String Errin = sc.nextLine();
-		System.out.print("Enter error message");
-=======
-		String input = JOptionPane.showInputDialog("Enter a number");
+		String input = sc.nextLine();
+		System.out.print("Enter a number");
 		int[] inBry = BinaryGenerate(input);
 		int[] HMcode = HammingCodeGenerate(inBry);
-		JOptionPane.showMessageDialog(null,
-		printArray("Bit Convertion: ", inBry) + "\n" + printArray("HammingCode: ",
-		HMcode));
-		String Errin = JOptionPane.showInputDialog("Enter error message");
->>>>>>> 8f2b72c262c242c35d1e06e5a90f0b4877a5e5fc
+		printArray("Bit Convertion: ", inBry);
+		printArray("HammingCode: ", HMcode);
+		String Errin = sc.nextLine();
+		System.out.print("Enter error message");
 		validate(Errin);
 		sc.close();
 	}
 
-	static public String printArray(String s, int[] a) {
+	static public void printArray(String s, int[] a) {
 		String str = (s + " ");
 		int i = 0;
 		while (i < a.length) {
 			str = str + a[i] + " ";
 			i++;
 		}
-		return str;
+		System.out.println(str);
 	}
 
 	public static int[] BinaryGenerate(String i) {
@@ -63,40 +52,46 @@ public class Calculator {
 		int[] hamming = new int[m + p];
 		int[] PCount = new int[p];
 		int count = 0;
+		int s = m + p - 1;
+		for (int h = 0; h < m + p; h++) {
+			hamming[h] = i[s];
+			s--;
+		}
 		for (int a = 0; a < ham.length; a++) {
-			if (parity(a,ham)) {
+			if (parity(a, ham)) {
 				PCount[count] = a;
 				count++;
 			} else {
-				ham[a] = i[a - count];
+				ham[a] = hamming[a - count];
 			}
 		}
 		for (int k = 0; k < p; k++) {
 			ham[PCount[k]] = EvenParity(PCount[k], ham);
 		}
-		int s = m + p - 1;
-		for (int h = 0; h < m + p; h++) {
-			hamming[h] = ham[s];
-			s--;
-		}
-		return hamming;
+		
+		return ham;
 	}
 
 	public static void validate(String str) {
 		int[] ham = new int[str.length()];
 		int err = -1;
 		int j = ham.length - 1;
-		for (int i = 0; i < ham.length; i++) {
-			ham[j] = str.charAt(i)-48;
-			j--;
-			//System.out.print(printArray("",ham));
+		for (int i = 0;; i++) {
+			if (Math.pow(2, j) >= ham.length) {
+				p = j;
+				break;
+			}
 		}
-		System.out.println(printArray("",ham));
 		for (int i = 0; i < ham.length; i++) {
-			if (parity(i,ham)) {
+			ham[j] = str.charAt(i) - 48;
+			j--;
+			// System.out.print(printArray("",ham));
+		}
+		//System.out.println(printArray("", ham));
+		for (int i = 0; i < ham.length; i++) {
+			if (parity(i, ham)) {
 				if (EvenParity(i, ham) == 1) {
-					err = i;
-					break;
+					err = err + i;
 				}
 			}
 		}
@@ -105,19 +100,13 @@ public class Calculator {
 		} else {
 			System.out.print("Received codes are invalid. Found error at " + (err + 1));
 			ham[err] = 1 - ham[err];
-			System.out.print(printArray("the Correct cide are ",ham));
+			printArray("\nthe Correct cide are ", ham);
 		}
 	}
 
-	public static boolean parity(int i,int[] str) {
-		int t;
-		for (int j = 0;; j++) {
-			if (Math.pow(2, j) >= str.length) {
-				t = j;
-				break;
-			}
-		}
-		for (int j = 0; j < t; j++) {
+	public static boolean parity(int i, int[] str) {
+
+		for (int j = 0; j < p; j++) {
 			if (i == Math.pow(2, j) - 1) {
 				return true;
 			}
@@ -128,7 +117,7 @@ public class Calculator {
 	public static int EvenParity(int py, int[] bry) {
 		int pt = 0;
 		for (int c = py; c < bry.length; c += 2 * (py + 1)) {
-			for (int j = c; j < c + py + 1; j++) {
+			for (int j = c; j < Math.min(c + py + 1,bry.length); j++) {
 				if (bry[j] == 1) {
 					pt++;
 				}
